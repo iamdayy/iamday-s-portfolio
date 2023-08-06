224ac3ab20eff59bc0a6d5b87c11e2c7375864c1
iamdayy <108299143+iamdayy@users.noreply.github.com>
2023-08-07 02:49:12 +0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import NavBar from './components/NavBar.vue';
+</script>
+
+<template>
+  <div class="min-h-full">
+    <div class="grid gap-2 p-2 background shadow-md">
+      <NavBar />
+      <header class="shadow">
+        <div class="border border-slate-500 rounded-2xl mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+          <div class="flex flex-wrap justify-around">
+            <img src="./assets/person.png" alt="i'am" class="max-w-md" />
+            <div class="bg-gray-600 bg-opacity-25 backdrop-blur-sm shadow-slate-900 shadow-inner rounded-xl max-w-2xl break-words p-3">
+              <h1 class="font-bold text-3xl md:text-5xl">Hello</h1>
+              <h2 class="text-5xl md:text-7xl font-semibold">I'am Dayyan Sauqy</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, nostrum? Recusandae optio ipsa, possimus et quidem amet repudiandae voluptates quos, eligendi adipisci blanditiis earum consectetur quia nobis! Dicta, voluptate voluptas.</p>
+            </div>
+          </div>
+        </div>
+      </header>
+    </div>
+    <main>
+      <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        <!-- Your content -->
+      </div>
+    </main>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/vue'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/vue/24/outline';
+import { ref } from 'vue';
+
+const opened = ref<boolean>(false);
+const navigation = [
+  { name: 'Dashboard', href: '#', current: true },
+  { name: 'Team', href: '#', current: false },
+  { name: 'Projects', href: '#', current: false },
+  { name: 'Calendar', href: '#', current: false },
+  { name: 'Reports', href: '#', current: false },
+]
+</script>
+<template>
+    <Disclosure as="nav" class="bg-gray-500 border bg-opacity-25 backdrop-blur-sm border-gray-50 border-opacity-30 shadow-md shadow-slate-950 w-11/12 rounded-full mx-auto mt-2" v-slot="{ open }" :class="opened ? 'rounded-none' : 'rounded-full'" >
+      <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div class="flex h-16 items-center justify-between">
+          <div class="flex items-center">
+            <div class="flex-shrink-0">
+              <img class="h-8 w-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
+            </div>
+            <div class="hidden md:block">
+              <div class="ml-10 flex items-baseline space-x-4">
+                <a v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? 'text-green-500 border border-green-500 hover:cursor-default font-bold' : 'text-gray-300 hover:border hover:border-slate-500 hover:text-slate', 'rounded-md px-3 py-2 text-sm font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</a>
+              </div>
+            </div>
+          </div>
+          <div class="flex items-center">
+            <div class="flex-shrink-0">
+              <h1 class="block md:hidden">Iamdayy</h1>
+            </div>
+          </div>
+          <div class="hidden md:block">
+            <div class="ml-4 flex items-center md:ml-6">
+              <button type="button" class="relative rounded-full p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                <span class="absolute -inset-1.5" />
+                <span class="sr-only">View notifications</span>
+                <BellIcon class="h-6 w-6" aria-hidden="true" />
+              </button>
+            </div>
+          </div>
+          <div class="-mr-2 flex md:hidden">
+            <!-- Mobile menu button -->
+            <DisclosureButton @click="opened = !opened" class="relative inline-flex items-center justify-center rounded-full p-2 text-gray-400 hover:text-white focus:outline-none focus:ring-1 focus:ring-white">
+              <span class="absolute -inset-0.5" />
+              <span class="sr-only">Open main menu</span>
+              <Bars3Icon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
+              <XMarkIcon v-else class="block h-6 w-6" aria-hidden="true" />
+            </DisclosureButton>
+          </div>
+        </div>
+      </div>
+
+      <DisclosurePanel class="md:hidden">
+        <div class="space-y-1 px-2 pb-3 pt-2 sm:px-3">
+          <DisclosureButton v-for="item in navigation" :key="item.name" as="a" :href="item.href" :class="[item.current ? 'text-green-500 border border-green-500 hover:cursor-default font-bold' : 'text-gray-300 hover:border hover:border-slate-500 hover:text-slate', 'block rounded-md px-3 py-2 text-base font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</DisclosureButton>
+        </div>
+        <div class="border-t border-gray-700 pb-3 pt-4">
+          <div class="flex items-center px-5">
+            <button type="button" class="relative ml-auto flex-shrink-0 rounded-full p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-1 focus:ring-white focus:ring-offset-1">
+              <span class="absolute -inset-1.5" />
+              <span class="sr-only">View notifications</span>
+              <BellIcon class="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+        </div>
+      </DisclosurePanel>
+    </Disclosure>
+</template>
+<style scoped></style>
